bf0b5976b8358d6a2d335938b241da0f028f64e2
user <user@aiuui-MacBookPro.local>
2026-09-24 02:14:56 +0900

    v3:54.19%

--- tests/run.sh
+++ tests/run.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
 cd "$(dirname "$0")"
 
-$MYGREP -V -y -n -b -2 -A1 -B3 -C "MATCH" data/context.txt
+$MYGREP -V -i -n -b -2 -A1 -B3 -C -e "MATCH" -e "foo" data/context.txt data/large.txt data/residue_context.txt
 $MYGREP -c -h -q -s -v "foo" data/coverage.txt data/empty.txt data/missing.txt
-$MYGREP -l "foo" data/coverage.txt data/empty.txt
+$MYGREP -c "foo" data/coverage.txt data/empty.txt
+$MYGREP -l "foo" data/missing.txt data/coverage.txt
 $MYGREP -L "foo" data/coverage.txt data/empty.txt
-$MYGREP "foo" data/missing.txt
-printf 'foo\nbar\n' | $MYGREP -c "foo"
-printf 'foo\n' | $MYGREP -l "foo"
-printf 'bar\n' | $MYGREP -L "foo"
-$MYGREP "foo" data/coverage.txt -n
-POSIXLY_CORRECT=1 $MYGREP "foo" data/coverage.txt -n
-$MYGREP -- "-foo" data/coverage.txt
-$MYGREP -f data/patterns_nonl.txt data/coverage.txt
-printf 'foo\nbar\n' | $MYGREP -f - data/coverage.txt
-$MYGREP -E -f data/ere_mega.txt data/classes.txt
-$MYGREP -E -i 'ABC|Def|[A-Z]+' data/engine.txt
-$MYGREP -E 'foo.*bar|[a-z]+[0-9]*' data/engine.txt
-$MYGREP -E '((ab|a)+|c?d*|[a-z]{2,}|(xy?){2,3})' data/engine.txt
+$MYGREP "foo" data
+printf 'foo!bar\nxfoo foo\n' | $MYGREP -G -G -w '\(foo!\|foo\)\(x*\)\2' - data/word_backtrack.txt
+$MYGREP -G -x '^\(ab\)\1$' data/engine.txt
+$MYGREP -E -E -w -f data/ere_mega.txt data/classes.txt
+$MYGREP -X egrep -x '(^a|b$)|(a^b)|(c$d)|([[:digit:]]+)' data/engine.txt
+$MYGREP -F -F -w "foo" data/words.txt
+$MYGREP -F -x "foo" data/words.txt
 $MYGREP -F -f data/fixed_many.txt data/engine.txt
-$MYGREP -F -i -e "foo" -e "BAR" -e "ALPHA" data/coverage.txt
-$MYGREP -F -f data/long_pattern.txt data/large.txt
-$MYGREP -G '^\(\(\w\+\)\|\(\W\+\)\|\([a-z]\{1,3\}\)\|\([^x]\+\)\|\(.\{1,4\}\)\)\1$' data/engine.txt
-$MYGREP -G '^\(\(a*\)\|\(b\+\)\|\(c\?\)\|\(d\{2,4\}\)\)\1$' data/engine.txt
-$MYGREP -G '^\(\(.\|..\)\{2,3\}\)\1$' data/engine.txt
-$MYGREP -G '^\([[:digit:]]\{1,3\}\)\1$' data/engine.txt
-$MYGREP -G '^\([^a-z]\+\)\1$' data/engine.txt
-$MYGREP -G '^\(\w\+\)\1$' data/engine.txt
-$MYGREP -X egrep '(^a|b$)|(a^b)|(c$d)|([[:digit:]]+)' data/engine.txt
-$MYGREP -F -E "foo" data/coverage.txt
+$MYGREP -E 'foo+bar' data/kw_nonexact.txt
+$MYGREP -G -f data/bre_mega.txt data/regex_edges.txt
+$MYGREP -G '^\(a*\)b\1$' data/regex_edges.txt
+$MYGREP -G '^\([a-c]*\)z\1$' data/regex_edges.txt
+$MYGREP -E 'a{0}|b{1}|c{1,3}|d{2,}|e{0,2}' data/engine.txt
+$MYGREP -G -f data/boundary_extend.txt data/empty.txt
+$MYGREP -G -f data/macro_extend.txt data/empty.txt
+printf '' | $MYGREP -G -f - data/nulls.txt
+$MYGREP -e "foo" -f data/patterns_nonl.txt data/coverage.txt
+$MYGREP -f data/missing_patterns.txt data/coverage.txt
 $MYGREP -E '[[:bogus:]]' data/coverage.txt
 $MYGREP -E '\' data/coverage.txt
+$MYGREP -G '\2' data/coverage.txt
+$MYGREP -G '\(' data/coverage.txt
+$MYGREP -Z "foo" data/coverage.txt
+$MYGREP -F -E "foo" data/coverage.txt
+$MYGREP -E -F "foo" data/coverage.txt
