ccdd2b069def06dd597ed578def628607030cc84
user <user@aiuui-MacBookPro.local>
2026-09-24 02:48:56 +0900

    61.59%

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
 $MYGREP -V -i -n -b -2 -A1 -B3 -C -e "MATCH" -e "foo" data/context.txt data/large.txt data/residue_context.txt
 $MYGREP -c -h -q -s -v "foo" data/coverage.txt data/empty.txt data/missing.txt
 $MYGREP -c "foo" data/coverage.txt data/empty.txt
@@ -16,18 +15,18 @@ $MYGREP -F -x "foo" data/words.txt
 $MYGREP -F -f data/fixed_many.txt data/engine.txt
 $MYGREP -E 'foo+bar' data/kw_nonexact.txt
 $MYGREP -G -f data/bre_mega.txt data/regex_edges.txt
-$MYGREP -E '^((ab|cd)+e?f*){2,4}$' data/engine.txt
+$MYGREP -E -e '^((ab|cd)+e?f*){2,4}$' -e 'a{2,}' -e 'b{0}' -e 'c{0,2}' data/engine.txt
 $MYGREP -G '^\<\(\w\{2,4\}\)\>\W\1$' data/runtime_regex.txt
 $MYGREP -G '^\(\w\)\B\w\b\W\1$' data/runtime_regex.txt
 $MYGREP -G -f data/boundary_extend.txt data/empty.txt
 $MYGREP -G -f data/macro_extend.txt data/empty.txt
 printf '' | $MYGREP -G -f - data/nulls.txt
 $MYGREP -e "foo" -f data/patterns_nonl.txt data/coverage.txt
-$MYGREP -f data/missing_patterns.txt data/coverage.txt
-$MYGREP -E '[[:bogus:]]' data/coverage.txt
-$MYGREP -E '\' data/coverage.txt
-$MYGREP -G '\2' data/coverage.txt
-$MYGREP -G '\(' data/coverage.txt
-$MYGREP -Z "foo" data/coverage.txt
-$MYGREP -G -f data/endbuf_pattern.txt data/runtime_regex.txt
+$MYGREP -F -f data/fixed_overlap.txt data/fixed_overlap_input.txt
+$MYGREP -G '^\([a-c]*d\)\1$' data/backtrack_maybe.txt
+$MYGREP -G '^\([^x]*x\)\1$' data/backtrack_maybe.txt
+$MYGREP -G '^\(f\)\1!\<.\>.*$' data/boundary_cases.txt
+$MYGREP -G '^\(a*\)*\1$' data/backtrack_maybe.txt
+$MYGREP -G '^\(\(a*\)b*\)\2$' data/backtrack_maybe.txt
+$MYGREP -G -f data/endbuf_pattern.txt data/end_ok.txt data/end_bad.txt
 printf 'foo\nbar\n' | $MYGREP -c "foo"
