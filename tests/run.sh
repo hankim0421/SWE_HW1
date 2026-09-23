3a0cae9ac16251e53e3b86f81f738faa6f476471
user <user@aiuui-MacBookPro.local>
2026-09-24 02:35:55 +0900

    57.96%

--- tests/run.sh
+++ tests/run.sh
@@ -9,16 +9,16 @@ $MYGREP -L "foo" data/coverage.txt data/empty.txt
 $MYGREP "foo" data
 printf 'foo!bar\nxfoo foo\n' | $MYGREP -G -G -w '\(foo!\|foo\)\(x*\)\2' - data/word_backtrack.txt
 $MYGREP -G -x '^\(ab\)\1$' data/engine.txt
-$MYGREP -E -E -w -f data/ere_mega.txt data/classes.txt
+$MYGREP -E -i 'FoO([[:alpha:]]|[[:upper:]]|[[:lower:]]|[[:digit:]]|[[:xdigit:]]|[[:space:]]|[[:punct:]]|[[:alnum:]]|[[:print:]]|[[:graph:]]|[[:cntrl:]])+' data/classmix.txt
 $MYGREP -X egrep -x '(^a|b$)|(a^b)|(c$d)|([[:digit:]]+)' data/engine.txt
 $MYGREP -F -F -w "foo" data/words.txt
 $MYGREP -F -x "foo" data/words.txt
 $MYGREP -F -f data/fixed_many.txt data/engine.txt
 $MYGREP -E 'foo+bar' data/kw_nonexact.txt
 $MYGREP -G -f data/bre_mega.txt data/regex_edges.txt
-$MYGREP -G '^\(a*\)b\1$' data/regex_edges.txt
-$MYGREP -G '^\([a-c]*\)z\1$' data/regex_edges.txt
-$MYGREP -E 'a{0}|b{1}|c{1,3}|d{2,}|e{0,2}' data/engine.txt
+$MYGREP -E '^((ab|cd)+e?f*){2,4}$' data/engine.txt
+$MYGREP -G '^\<\(\w\{2,4\}\)\>\W\1$' data/runtime_regex.txt
+$MYGREP -G '^\(\w\)\B\w\b\W\1$' data/runtime_regex.txt
 $MYGREP -G -f data/boundary_extend.txt data/empty.txt
 $MYGREP -G -f data/macro_extend.txt data/empty.txt
 printf '' | $MYGREP -G -f - data/nulls.txt
@@ -29,5 +29,5 @@ $MYGREP -E '\' data/coverage.txt
 $MYGREP -G '\2' data/coverage.txt
 $MYGREP -G '\(' data/coverage.txt
 $MYGREP -Z "foo" data/coverage.txt
-$MYGREP -F -E "foo" data/coverage.txt
-$MYGREP -E -F "foo" data/coverage.txt
+$MYGREP -G -f data/endbuf_pattern.txt data/runtime_regex.txt
+printf 'foo\nbar\n' | $MYGREP -c "foo"
